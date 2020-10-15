23f08ea8f34fd09b5c873254a21c766c5b790b44
Dorota Dabrowska-Kowalska <dorota.dabrowska.kowalska@gmail.com>
2020-10-15 12:56:11 +0200

    Main routing and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CustomerPanelComponent } from './components/customer/customer-panel/customer-panel.component';
+import { OwnerPanelComponent } from './components/owner/owner-panel/owner-panel.component';
+import { LoginComponent } from './components/shared/login/login.component';
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'owner',
+    component: OwnerPanelComponent,
+    // canActivate: [],
+  },
+  {
+    path: 'customer',
+    component: CustomerPanelComponent,
+    // canActivate: [],
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+
 import { OwnerPanelComponent } from './components/owner/owner-panel/owner-panel.component';
 import { CustomerListComponent } from './components/owner/owner-panel/customers/customer-list/customer-list.component';
 import { CustomerDetailsComponent } from './components/owner/owner-panel/customers/customer-details/customer-details.component';
@@ -18,11 +22,16 @@ import { OrdersComponent } from './components/owner/owner-panel/orders/orders.co
 import { OrderListComponent } from './components/owner/owner-panel/orders/order-list/order-list.component';
 import { OrderComponent } from './components/owner/owner-panel/orders/order-list/order/order.component';
 import { CustomerPanelComponent } from './components/customer/customer-panel/customer-panel.component';
-import { CustomerIcecreamListComponent } from './components/customer/customer-panel/customer-icecream-list/customer-icecream-list.component';
+import {
+  CustomerIcecreamListComponent } from './components/customer/customer-panel/customer-icecream-list/customer-icecream-list.component';
 import { CustomerIcecreamComponent } from './components/customer/customer-panel/customer-icecream-list/customer-icecream/customer-icecream.component';
 import { CustomerOrderListComponent } from './components/customer/customer-panel/customer-order-list/customer-order-list.component';
 import { CustomerOrderComponent } from './components/customer/customer-panel/customer-order-list/customer-order/customer-order.component';
 import { HeaderComponent } from './components/shared/header/header.component';
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/shared/login/login.component';
+
+
 
 @NgModule({
   declarations: [
@@ -45,12 +54,17 @@ import { HeaderComponent } from './components/shared/header/header.component';
     CustomerIcecreamComponent,
     CustomerOrderListComponent,
     CustomerOrderComponent,
-    HeaderComponent
+    HeaderComponent,
+    PageNotFoundComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
